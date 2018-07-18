5f65a202c4551a89d93cb3ec0c2fdfd5e2f612fa
edz <edz@edzdeMacBook-Pro.local>
2018-07-18 14:16:09 +0800

    d 22

--- android-app/src/net/oschina/app/AppException.java
+++ android-app/src/net/oschina/app/AppException.java
@@ -37,7 +37,7 @@ public class AppException extends Exception implements UncaughtExceptionHandler{
 	public final static byte TYPE_HTTP_ERROR= 0x02;
 	public final static byte TYPE_XML	 	= 0x05;
 	public final static byte TYPE_IO	 	= 0x03;
-	public final static byte TYPE_RUN	 	= 0x07;
+	public final static byte TYPE_RUN	 	= 0x077;
 	
 	private byte type;
 	private int code;
